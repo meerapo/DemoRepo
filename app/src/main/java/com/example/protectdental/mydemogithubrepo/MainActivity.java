4055cf06621a1ec504539bc23feeee01aa1d037d
meerapo <42575281+meerapo@users.noreply.github.com>
2018-08-23 12:09:29 +0530

    See the Remote changes
    See the Remote changes added one comment

--- app/src/main/java/com/example/protectdental/mydemogithubrepo/MainActivity.java
+++ app/src/main/java/com/example/protectdental/mydemogithubrepo/MainActivity.java
@@ -30,5 +30,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         Intent intent=new Intent(this,OneActivity.class);
         startActivity(intent);
+           //its working now
     }
 }
