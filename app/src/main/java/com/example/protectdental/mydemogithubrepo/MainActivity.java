2ded6c39c583bdfa382bd5c6c4b54c60c85606e1
PROTECT DENTAL <meera@intelehealth.io>
2018-08-23 11:41:27 +0530

    Added oneActivity..

--- app/src/main/java/com/example/protectdental/mydemogithubrepo/MainActivity.java
+++ app/src/main/java/com/example/protectdental/mydemogithubrepo/MainActivity.java
@@ -1,16 +1,20 @@
 package com.example.protectdental.mydemogithubrepo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+        Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        button=(Button)findViewById(R.id.button1);
+        button.setOnClickListener(this);
         creatTask();
     }
 
@@ -20,5 +24,11 @@ public class MainActivity extends AppCompatActivity {
         //meera
         //How are you?
     //Want to update new comments
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent=new Intent(this,OneActivity.class);
+        startActivity(intent);
     }
 }
