70d198142572523e3c2a78e424bf7d39f357d555
PROTECT DENTAL <meera@intelehealth.io>
2018-08-23 13:22:08 +0530

    Added comment in OneActivity

--- app/src/main/java/com/example/protectdental/mydemogithubrepo/OneActivity.java
+++ app/src/main/java/com/example/protectdental/mydemogithubrepo/OneActivity.java
@@ -9,5 +9,6 @@ public class OneActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one);
+        //Hey Click me to see magic
     }
 }
